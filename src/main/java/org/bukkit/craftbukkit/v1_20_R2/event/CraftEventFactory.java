db16080604da632b4016f957ebc49e84140d4eb4
1798643961 <1798643961@qq.com>
2023-09-30 14:01:34 +0800

    Update upstream

--- src/main/java/org/bukkit/craftbukkit/v1_20_R2/event/CraftEventFactory.java
+++ src/main/java/org/bukkit/craftbukkit/v1_20_R2/event/CraftEventFactory.java
@@ -1520,6 +1520,8 @@ public class CraftEventFactory {
                 }
                 CraftMetaBook meta = (CraftMetaBook) editBookEvent.getNewBookMeta();
                 CraftItemStack.setItemMeta(itemInHand, meta);
+            } else {
+                player.getBukkitEntity().updateInventory(); // SPIGOT-7484
             }
         }
 
